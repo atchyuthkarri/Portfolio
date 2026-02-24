fbc4999fe91025082511a9db155381e34ea122f3
atchyuthkarri <atchyuthkarri46@gmail.com>
2026-02-24 12:11:34 +0530

    data updated

--- app/_components/Banner.tsx
+++ app/_components/Banner.tsx
@@ -84,7 +84,7 @@ const Banner = () => {
                     </div>
                     <div className="slide-up-and-fade">
                         <h5 className="text-3xl sm:text-4xl font-anton text-primary mb-1.5">
-                            1K+
+                            10K+
                         </h5>
                         <p className="text-muted-foreground">Hours Worked</p>
                     </div>
